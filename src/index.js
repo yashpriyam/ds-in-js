3056065dfc823d9e69e5154d1f8601af909564e7
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2021-03-29 01:00:39 +0530

    doubly-linked-list add print method to doubly linked list as in singly linked list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { SinglyLinkedList } from "./singly-linked-list";
+import { DoublyLinkedList } from "./doubly-linked-list";
 import "./styles.css";
 
 const sllDomref = document.getElementById("singly-linkedlist");
@@ -12,3 +13,14 @@ const sllTextNode = document.createTextNode(
   `Singly Linked List Values: [${list.print()}]`
 );
 sllDomref.appendChild(sllTextNode);
+
+const dlistDomref = document.getElementById("doubly-linkedlist");
+const dlist = new DoublyLinkedList();
+dlist.push("Harry");
+dlist.push("Ron");
+dlist.push("Hermione");
+dlist.push("Dumbledore");
+const dlistTextNode = document.createTextNode(
+  `Doubly Linked List Values: [${dlist.print()}]`
+);
+dlistDomref.appendChild(dlistTextNode);
